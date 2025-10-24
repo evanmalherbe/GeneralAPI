4c3b9159abf9646c6cdbe98990857d99699ed8cb
Evan <evan@ix.co.za>
2025-10-24 09:23:16 +0200

    Removed duplicated files from commit

--- TransferObjects/AboutResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeneralAPI.TransferObjects
-{
-	public class AboutResponseDTO
-	{
-		public string? AboutText { get; set; }
-		public List<EducationResponseDTO> Education { get; set; }
-		public List<WorkExperienceResponseDTO> Work { get; set; }
-	}
-}
